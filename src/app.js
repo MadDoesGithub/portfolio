5b251de6b80e158e8c6a92387eb39b3f0bf5aac5
BuildTools <unconfigured@null.spigotmc.org>
2021-11-17 21:40:19 +0300

    removed react-router

--- src/app.js
+++ src/app.js
@@ -2,15 +2,6 @@ import React  from "react";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Card from './components/Card';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  Redirect
-} from "react-router-dom";
-import Contact from '../pages/Contact';
-
 
 
 const App = props => {
