bc989fd0152aa2eecf79b38a7964401b15893230
Mad <63809553+MadDoesGithub@users.noreply.github.com>
2021-11-18 06:36:16 +0300

    new modal added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,20 @@ import React from "react";
 
 function Footer() {
   return (
-    <><div class="mockup-code">
-          <pre data-prefix="$">
-              <code>© Imad, 2021.</code>
-          </pre>
-          <pre data-prefix=">" class="text-warning">
-              <code>All rights reserved</code>
-          </pre>
-          <pre data-prefix=">" class="text-success">
-              <code>Made with 🖤</code>
-          </pre>
-      </div></> 
-        
+    <>
+      <div class="mockup-code">
+        <pre data-prefix="$">
+          <code>© Imad, 2021.</code>
+        </pre>
+        <pre data-prefix=">" class="text-warning">
+          <code>All rights reserved</code>
+        </pre>
+        <pre data-prefix=">" class="text-success">
+          <input type="checkbox" id="secret" class="modal-toggle" />
+          <code>Made with </code> <a class="" href="#secret">🖤</a>
+        </pre>
+      </div>
+    </>
   );
 }
 
